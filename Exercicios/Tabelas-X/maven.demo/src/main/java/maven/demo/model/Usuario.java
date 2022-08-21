b90f7cc078db8aec561419fa24a738593cbead02
Fernando Campos Silva Dal' Maria <fernando.csm123@gmail.com>
2022-08-21 20:32:37 -0300

    exercicio-2

---
+++ Exercicios/Tabelas-X/maven.demo/src/main/java/maven/demo/model/Usuario.java
@@ -0,0 +1,72 @@
+package maven.demo.model;
+
+import maven.demo.dao.UsuarioDAO;
+
+public class Usuario {
+	private static UsuarioDAO uDAO = new UsuarioDAO();
+	private int codigo;
+	private String login;
+	private String senha;
+	private char sexo;
+	private static int nextCode = uDAO.getLastCode() + 1;
+	
+	public Usuario() {
+		this.codigo = -1;
+		this.login = "";
+		this.senha = "";
+		this.sexo = '*';
+	}
+	
+	public Usuario(int codigo, String login, String senha, char sexo) {
+		this.codigo = codigo;
+		this.login = login;
+		this.senha = senha;
+		this.sexo = sexo;
+	}
+
+	public static int getNextCode() {
+		return nextCode;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo() {
+		this.codigo = getNextCode();
+		increment();
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public static void increment() {
+		nextCode++;
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuario [codigo=" + codigo + ", login=" + login + ", senha=" + senha + ", sexo=" + sexo + "]";
+	}	
+}
