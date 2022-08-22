8716dc4ea9f6c0bd89c8ba4335706b332e569abd
Fernando Campos Silva Dal' Maria <fernando.csm123@gmail.com>
2022-08-22 19:09:25 -0300

    referencia

--- Exercicios/Tabelas-X/maven.demo/src/main/java/maven/demo/app/Aplicacao.java
+++ Exercicios/Tabelas-X/maven.demo/src/main/java/maven/demo/app/Aplicacao.java
@@ -6,6 +6,10 @@ import maven.demo.dao.DAO;
 import maven.demo.dao.UsuarioDAO;
 import maven.demo.model.Usuario;
 
+/**
+ * Código retirado de https://github.com/icei-pucminas/ti2cc.
+ * Adaptação por Fernando Campos Silva Dal Maria
+ */
 public class Aplicacao {
 	
 	public static void main(String[] args) throws Exception {
@@ -13,13 +17,13 @@ public class Aplicacao {
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		
 		System.out.println("\n\n==== Inserir usuário === ");
-		Usuario usuario = new Usuario(11, "pablo", "pablo",'M');
+		Usuario usuario = new Usuario(12, "fernando", "fernando",'M');
 		if(usuarioDAO.insert(usuario) == true) {
 			System.out.println("Inserção com sucesso -> " + usuario.toString());
 		}
 		
 		System.out.println("\n\n==== Testando autenticação ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", "pablo"));
+		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("fernando", "fernando"));
 			
 		System.out.println("\n\n==== Mostrar usuários do sexo masculino === ");
 		List<Usuario> usuarios = usuarioDAO.getSexoMasculino();
@@ -28,14 +32,14 @@ public class Aplicacao {
 		}
 
 		System.out.println("\n\n==== Atualizar senha (código (" + usuario.getCodigo() + ") === ");
-		usuario.setSenha(DAO.toMD5("pablo"));
+		usuario.setSenha(DAO.toMD5("fernando"));
 		usuarioDAO.update(usuario);
 		
 		System.out.println("\n\n==== Testando autenticação ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", DAO.toMD5("pablo")));		
+		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("fernando", DAO.toMD5("fernando")));		
 		
 		System.out.println("\n\n==== Invadir usando SQL Injection ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", "x' OR 'x' LIKE 'x"));
+		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("fernando", "x' OR 'x' LIKE 'x"));
 
 		System.out.println("\n\n==== Mostrar usuários ordenados por código === ");
 		usuarios = usuarioDAO.getOrderByCodigo();
